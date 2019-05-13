84b96afaffdacf5d63fe2b8dd481dfb53afb6e50
shuang6 <319825685@qq.com>
2019-05-13 12:21:58 +0800

    Learning Text Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {
-  runApp(MyApp());
-}
+main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext ctx) {
     return MaterialApp(
-      title: 'Welcome to flutter',
+      title: 'Text Widget',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello Bar'),
-        ),
         body: Center(
-          child: Text('Hello.'),
+          child: Text(
+            'Hello. I like fe. 我希望有个如你一般的人。如这山间清晨一般明亮清爽的人,如奔赴古城道路上阳光一般的人,温暖而不炙热,覆盖我所有肌肤。',
+            textAlign: TextAlign.center,
+            maxLines: 1,
+            overflow: TextOverflow.fade,
+            style: TextStyle(
+              fontSize: 25.0,
+              color: Color.fromARGB(255, 255, 125, 125),
+              decoration: TextDecoration.underline,
+              decorationStyle: TextDecorationStyle.solid,
+            ),
+          ),
         ),
-      )
+      ),
     );
   }
 }
