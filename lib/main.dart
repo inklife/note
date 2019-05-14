65394b4999f34cccad1ed09b344b963d63a0c807
shuang6 <319825685@qq.com>
2019-05-14 18:36:43 +0800

    Learning Image Widget

--- lib/main.dart
+++ lib/main.dart
@@ -11,23 +11,17 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: Center(
           child: Container(
-            child: new Text(
-              'Hello fe.',
-              style: TextStyle(fontSize: 40.0)
+            child: new Image.network(
+              'https://www.baidu.com/img/bd_logo1.png?where=super',
+              color: Colors.greenAccent,
+              colorBlendMode: BlendMode.colorDodge,
+              repeat: ImageRepeat.noRepeat,
+              // fit: BoxFit.scaleDown,
             ),
-            alignment: Alignment.topLeft,
-            width: 500.0,
-            height: 400.0,
-            // color: Colors.blue,
-            padding: const EdgeInsets.fromLTRB(10.0, 30.0, 0.0, 0.0),
-            margin: const EdgeInsets.all(10.0),
-            decoration: new BoxDecoration(
-              gradient: const LinearGradient(
-                colors: [ Colors.lightBlue, Colors.greenAccent, Colors.yellow ],
-              ),
-              border: Border.all(width: 2.0, color: Colors.lime)
-            ),
-          )
+            width: 300.0,
+            height: 200.0,
+            color: Colors.lightBlue,
+          ),
         ),
       ),
     );
