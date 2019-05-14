1446dc12220cbb3b81fce5d12fcfddb2976bb7dc
shuang6 <319825685@qq.com>
2019-05-14 19:05:17 +0800

    Learning ListView Part One

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
-main() => runApp(MyApp());
+main(List<String> args) {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext ctx) {
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Text Widget',
+      title: 'Hello, MD App.',
       home: Scaffold(
-        body: Center(
-          child: Container(
-            child: new Image.network(
-              'https://www.baidu.com/img/bd_logo1.png?where=super',
-              color: Colors.greenAccent,
-              colorBlendMode: BlendMode.colorDodge,
-              repeat: ImageRepeat.noRepeat,
-              // fit: BoxFit.scaleDown,
+        appBar: new AppBar(title: new Text('hahha')),
+        body: new ListView(
+          children: <Widget>[
+            new ListTile(
+              leading: new Icon(Icons.ac_unit),
+              title: new Text('ac_unit '),
             ),
-            width: 300.0,
-            height: 200.0,
-            color: Colors.lightBlue,
-          ),
-        ),
+            new ListTile(
+              leading: new Icon(Icons.access_alarm),
+              title: new Text('access_alarm '),
+            ),
+            new ListTile(
+              leading: new Icon(Icons.account_balance),
+              title: new Text('account_balance '),
+            ),
+            new Image.network('https://www.baidu.com/img/bd_logo1.png?where=super'),
+            new Text(
+              'Some datas',
+              style: TextStyle(fontSize: 32.0, color: Colors.lightBlue),
+            ),
+            new Image.network('https://photo.tuchong.com/2311463/f/15113534.jpg'),
+          ],
+        )
       ),
     );
   }
+  
 }
